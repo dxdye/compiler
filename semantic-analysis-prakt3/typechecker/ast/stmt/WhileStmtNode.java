15769b17d2f9033128f69766fb61bc7dd085b138
dxdye <s-tbertr@haw-landshut.de>
2025-12-21 00:10:29 +0100

    fix: classpath

--- semantic-analysis-prakt3/typechecker/ast/stmt/WhileStmtNode.java
+++ semantic-analysis-prakt3/typechecker/ast/stmt/WhileStmtNode.java
@@ -1,13 +1,8 @@
 package ast.stmt;
 
-import support.*;
 import ast.base.*;
-import ast.decl.*;
-import ast.type.*;
-import ast.stmt.*;
 import ast.expr.*;
 import java.io.*;
-import java.util.*;
 
 public class WhileStmtNode extends StmtNode {
     public WhileStmtNode(ExpNode exp, StmtListNode slist) {
